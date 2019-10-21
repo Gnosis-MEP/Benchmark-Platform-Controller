699e1bf56e405f6df239ddd9b745d9c72e30c859
Felipe Arruda Pontes <felipe.arruda.pontes@insight-centre.org>
2019-10-21 14:06:35 +0100

    adding gpu and non-gpu support

--- scripts/start_target_system.sh
+++ scripts/start_target_system.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 DIR="$(cd -P "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
+if [ "$USE_GPU" == "1" ]
+then
+    COMPOSE_SCRIPT=compose-all.sh
+else
+    COMPOSE_SCRIPT=compose-media.sh
+fi
+
 cd ${DATA_DIR}/mps-node
 source load_env.sh
 
-./compose-all.sh -f ${TARGET_COMPOSE_OVERRIDE_FILENAME} pull
-./compose-all.sh -f ${TARGET_COMPOSE_OVERRIDE_FILENAME} up -d
+./${COMPOSE_SCRIPT} -f ${TARGET_COMPOSE_OVERRIDE_FILENAME} pull
+./${COMPOSE_SCRIPT} -f ${TARGET_COMPOSE_OVERRIDE_FILENAME} up -d
+
 # docker-compose -f docker-compose.yml -f docker-compose-with-gpu-obj-detection.yml -f ${TARGET_COMPOSE_OVERRIDE_FILENAME} pull
 
 # docker-compose -f docker-compose.yml -f docker-compose-with-gpu-obj-detection.yml -f ${TARGET_COMPOSE_OVERRIDE_FILENAME} up -d
