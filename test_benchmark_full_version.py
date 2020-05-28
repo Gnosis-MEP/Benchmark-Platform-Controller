7a3c81bfcb782704adb13e5feaa782f543875de4
Felipe Arruda Pontes <felipe.arruda.pontes@insight-centre.org>
2020-05-28 16:30:05 +0100

    adding improved full versio bm script

--- test_benchmark_full_version.py
+++ test_benchmark_full_version.py
@@ -77,8 +77,12 @@ def check_results(base_url, result_id, start_time):
 
 def run(base_url, tag, start_time):
     run_benchmark_url = build_url(base_url, RUN_BENCHMARK_ENDPOINT)
-    params = {'override_services': {}, 'target_system': {'version': tag}}
+    if tag:
+        params = {'override_services': {}, 'target_system': {'version': tag}}
+    else:
+        params = {'override_services': {}}
     print(f'Sending requests for benchmark on url "{run_benchmark_url}" with params: {params}')
+
     res = make_request_post(run_benchmark_url, params)
     if res.status == 200:
         content = res.read().decode('utf8')
@@ -109,10 +113,35 @@ def make_request_post(url, data):
     return response
 
 
+def print_average_results(results):
+    avgs = []
+    stds = []
+    for result in results:
+        avg = result['evaluations']['benchmark_tools.evaluation.latency_evaluation']['latency_avg']['value']
+        std = result['evaluations']['benchmark_tools.evaluation.latency_evaluation']['latency_std']['value']
+        avgs.append(avg)
+        stds.append(std)
+
+    avg_avg = sum(avgs) / len(results)
+    avg_std = sum(stds) / len(results)
+    print("Final average of values from all benchmarks:")
+    print(f"avg_avg: {avg_avg}")
+    print(f"avg_std: {avg_std}")
+
+
 if __name__ == '__main__':
     benchmark_platform_controller_url = sys.argv[1]
     tag = sys.argv[2]
-    start_time = datetime.datetime.now()
-    result = run(benchmark_platform_controller_url, tag, start_time)
-    print(result)
+    total_benchs = 1
+    if len(sys.argv) == 4:
+        total_benchs = int(sys.argv[3])
+
+    results = []
+    for i in range(total_benchs):
+        start_time = datetime.datetime.now()
+        result = run(benchmark_platform_controller_url, tag, start_time)
+        time.sleep(5)
+        results.append(result)
+    print(results)
+    print_average_results(results)
     exit(0)
