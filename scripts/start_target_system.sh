904b55e4ce9878dd1706ccd2758102509612002a
Felipe Arruda Pontes <felipe.arruda.pontes@gmail.com>
2021-02-11 15:09:16 +0000

    finishing the setup/start/stop for extra node (jetson)

--- scripts/start_target_system.sh
+++ scripts/start_target_system.sh
@@ -19,6 +19,9 @@ sleep 1
 echo "checking for extra nodes..."
 if [ -f "${DATA_DIR}/${EXTRANODE_JSON_CONFIG_FILENAME}" ]; then
     echo "${DATA_DIR}/${EXTRANODE_JSON_CONFIG_FILENAME} exists. Will setup and start extra node"
+    cd ${DIR}/../ansible-files/
+    ansible-playbook -i inventory.ini target-system.yml -e "@${DATA_DIR}/${EXTRANODE_JSON_CONFIG_FILENAME}" -e gitlab_user=${GITLAB_USER} -e gitlab_pass=${GITLAB_PASS} -l extra --tags "setup,start"
+    cd ${DATA_DIR}/mps-node
 else
     echo "${DATA_DIR}/${EXTRANODE_JSON_CONFIG_FILENAME} does not exists."
 fi
--- scripts/stop_target_system.sh
+++ scripts/stop_target_system.sh
@@ -4,4 +4,13 @@ DIR="$(cd -P "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd ${DATA_DIR}/mps-node
 source load_env.sh
 
+echo "checking for extra nodes..."
+if [ -f "${DATA_DIR}/${EXTRANODE_JSON_CONFIG_FILENAME}" ]; then
+    echo "${DATA_DIR}/${EXTRANODE_JSON_CONFIG_FILENAME} exists. Will stop extra node"
+    cd ${DIR}/../ansible-files/
+    ansible-playbook -i inventory.ini target-system.yml -e "@${DATA_DIR}/${EXTRANODE_JSON_CONFIG_FILENAME}" -e gitlab_user=${GITLAB_USER} -e gitlab_pass=${GITLAB_PASS} -l extra --tags "stop"
+    cd ${DATA_DIR}/mps-node
+else
+    echo "${DATA_DIR}/${EXTRANODE_JSON_CONFIG_FILENAME} does not exists."
+fi
 ./compose-all.sh -f ${TARGET_COMPOSE_OVERRIDE_FILENAME} down
